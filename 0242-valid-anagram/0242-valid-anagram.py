776d3b570df6a33944564786a0d9c56fd43d314f
Kaushal prasad <111481878+kaushal354@users.noreply.github.com>
2025-05-30 09:11:33 +0530

    Time: 29 ms (12%), Space: 12.5 MB (96.67%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,22 +1,5 @@
+from collections import Counter
 
 class Solution(object):
     def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-        freq1 = sorted(s)
-        freq2 = sorted(t)
-        if len(freq1) != len(freq2):
-            return False
-        else:
-            for i in range(len(freq1)):
-                if freq1[i] != freq2[i]:
-                    return False
-        return True
-
-nums = "anagram"
-nums1 = "nagaram"
-p = Solution()
-print(p.isAnagram(nums,nums1))
+        return Counter(s) == Counter(t)
