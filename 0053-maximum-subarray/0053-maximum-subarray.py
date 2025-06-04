c6642b4cae3a3063d19d040609b5fcdbefb264e3
Kaushal prasad <111481878+kaushal354@users.noreply.github.com>
2025-06-04 16:55:21 +0530

    Time: 105 ms (10.6%), Space: 21 MB (97.1%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.py
+++ 0053-maximum-subarray/0053-maximum-subarray.py
@@ -1,16 +1,8 @@
-#kedans algo
 class Solution(object):
     def maxSubArray(self, nums):
-        max_sum = float('-inf')
-        current_sum = 0 
-        
-        for num in nums:
+        current_sum = max_sum = nums[0]
+        for num in nums[1:]:
             current_sum = max(num, current_sum + num) 
             max_sum = max(max_sum, current_sum) 
         
         return max_sum
-
-# Example usage:
-nums = [-2, 1, -3, 4, -1, 2, 1, -5, 4]
-solution = Solution()
-print(solution.maxSubArray(nums))  # Output: 6
