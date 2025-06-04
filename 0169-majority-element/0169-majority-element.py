31344cf7187fbb6081b3c9e18b9d64e77ed304bb
Kaushal prasad <111481878+kaushal354@users.noreply.github.com>
2025-06-04 17:07:14 +0530

    Time: 10 ms (55.6%), Space: 13.8 MB (31.23%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -5,9 +5,16 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        freq = Counter(nums)
-        nums3 =  max(freq, key = freq.get)
-        return nums3
+        count = 0
+        candidate = None
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += (1 if num == candidate else -1)
+
+        return candidate
+
 
 
          
